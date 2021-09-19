10d074254afd23814ac0113dd7214f3a53f00815
Sergej Garkusha <Tenebrann@googlemail.com>
2021-09-19 05:35:15 +0200

    add CheckEmail Page

--- src/modules/routes/Routes.tsx
+++ src/modules/routes/Routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { Login } from '../pages/Auth/Login/Login';
+import { CheckEmail } from '../pages/Auth/LostPassword/CheckEmail/CheckEmail';
 import { LostPassword } from '../pages/Auth/LostPassword/LostPassword';
 import { NewPassword } from '../pages/Auth/NewPassword/NewPassword';
 import { Registration } from '../pages/Auth/Registration/Registration';
@@ -15,6 +16,7 @@ export enum PATH {
   REGISTRATION = '/registration',
   RESET_PASSWORD = '/resetpass',
   NEW_PASS = '/newpass',
+  CHECK_EMAIL = '/checkemail',
 }
 
 export const Routes = () => {
@@ -27,6 +29,8 @@ export const Routes = () => {
       <Route path={PATH.REGISTRATION} component={Registration} />
       <Route path={PATH.RESET_PASSWORD} component={LostPassword} />
       <Route path={PATH.NEW_PASS} component={NewPassword} />
+      <Route path={PATH.NEW_PASS} component={NewPassword} />
+      <Route path={PATH.CHECK_EMAIL} component={CheckEmail} />
       <Route component={PageNotFound} />
     </Switch>
   );
