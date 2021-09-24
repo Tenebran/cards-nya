d7bef54eab73927164f28317ea8145bba1839538
Sergej Garkusha <Tenebrann@googlemail.com>
2021-09-24 02:35:47 +0200

    fix status

--- src/modules/pages/Auth/LostPassword/LostPassword.tsx
+++ src/modules/pages/Auth/LostPassword/LostPassword.tsx
@@ -1,30 +1,36 @@
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 import SuperButton from '../../../components/SuperButton/SuperButton';
 import SuperEditableSpan from '../../../components/SuperEditableSpan/SuperEditableSpan';
+import { forgotPasswordThunk } from '../../../redux/reducers/authReducer';
 import { AppStoreType } from '../../../redux/store';
 import { PATH } from '../../../routes/Routes';
 import './LostPassword.scss';
 
 export const LostPassword = () => {
   const statusSend = useSelector<AppStoreType, boolean>(state => state.user.statusSend);
-  const [value, setValue] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const dispatch = useDispatch();
+
+  const resetPasswordHandler = () => {
+    dispatch(forgotPasswordThunk(email));
+  };
 
   if (statusSend) {
     return <Redirect to={PATH.CHECK_EMAIL} />;
   }
 
   return (
-    <div className="forgot">
+    <form className="forgot">
       <div className="forgot__wrapper">
         <h2 className="forgot__title">It-Incubator</h2>
         <span className="forgot__subtitle">Forgot your password?</span>
         <div>
           <SuperEditableSpan
-            value={value}
-            onChangeText={setValue}
-            spanProps={{ children: value ? undefined : 'Email' }}
+            value={email}
+            onChangeText={setEmail}
+            spanProps={{ children: email ? undefined : 'Email' }}
             inputName="Email"
             type={'email'}
           />
@@ -33,13 +39,17 @@ export const LostPassword = () => {
           Enter your email address and we will send you further instructions
         </div>
 
-        <SuperButton name="Send Instructions" buttonWidth="266px" />
+        <SuperButton
+          name="Send Instructions"
+          buttonWidth="266px"
+          onClickHandler={resetPasswordHandler}
+        />
 
         <div className="forgot__remember">Did you remember your password?</div>
         <Link to={PATH.LOGIN} className="forgot__tryloggin">
           Try logging in
         </Link>
       </div>
-    </div>
+    </form>
   );
 };
