8a3245e5f57832af8cfd895058c9285f9035c29a
Sergej Garkusha <Tenebrann@googlemail.com>
2021-10-03 09:11:03 +0200

    change api

--- src/modules/api/api.ts
+++ src/modules/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { resetEmailApi } from './messageStyle';
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:7542/2.0',
-  // baseURL: 'https://neko-back.herokuapp.com/2.0',
+  // baseURL: 'http://localhost:7542/2.0',
+  baseURL: 'https://neko-back.herokuapp.com/2.0',
   withCredentials: true,
 });
