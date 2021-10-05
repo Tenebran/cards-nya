3a1756c826928725fe186c895d6302415de36c48
Sergej Garkusha <Tenebrann@googlemail.com>
2021-10-05 21:30:34 +0200

    add range slider

--- src/modules/pages/CardsPack/CardsShow/CardsShow.tsx
+++ src/modules/pages/CardsPack/CardsShow/CardsShow.tsx
@@ -1,7 +1,48 @@
+import { Box, Slider } from '@mui/material';
 import React, { useState } from 'react';
 import './CardsShow.scss';
 
+function valuetext(value: number) {
+  return `${value}°C`;
+}
+
+const minDistance = 10;
+
 export const CardsShow = (props: PropsType) => {
+  const [value1, setValue1] = React.useState<number[]>([20, 37]);
+
+  const handleChange1 = (event: Event, newValue: number | number[], activeThumb: number) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (activeThumb === 0) {
+      setValue1([Math.min(newValue[0], value1[1] - minDistance), value1[1]]);
+    } else {
+      setValue1([value1[0], Math.max(newValue[1], value1[0] + minDistance)]);
+    }
+  };
+
+  const [value2, setValue2] = React.useState<number[]>([20, 37]);
+
+  const handleChange2 = (event: Event, newValue: number | number[], activeThumb: number) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (newValue[1] - newValue[0] < minDistance) {
+      if (activeThumb === 0) {
+        const clamped = Math.min(newValue[0], 100 - minDistance);
+        setValue2([clamped, clamped + minDistance]);
+      } else {
+        const clamped = Math.max(newValue[1], minDistance);
+        setValue2([clamped - minDistance, clamped]);
+      }
+    } else {
+      setValue2(newValue as number[]);
+    }
+  };
+
   return (
     <>
       <span className="cards-pack__show_title">Show packs cards</span>
@@ -24,6 +65,16 @@ export const CardsShow = (props: PropsType) => {
         </button>
       </div>
       <span className="cards-pack__show_title">Number of cards</span>
+      <Box sx={{ width: '80%' }}>
+        <Slider
+          getAriaLabel={() => 'Minimum distance shift'}
+          value={value2}
+          onChange={handleChange2}
+          valueLabelDisplay="auto"
+          getAriaValueText={valuetext}
+          disableSwap
+        />
+      </Box>
     </>
   );
 };
